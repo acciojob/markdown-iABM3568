ea886100a84dfdac4d1ecccc2db24b5a3736d636
Abhishek Mishra <iabm3568@gmail.com>
2026-01-02 20:38:41 +0530

    Update MarkdownEditor.js

--- src/components/MarkdownEditor.js
+++ src/components/MarkdownEditor.js
@@ -1,22 +1,2 @@
-import React from "react";
-
-const MarkdownEditor = ({ markdown, setMarkdown, preview }) => {
-  return (
-    <div style={{ display: "flex", height: "100vh" }}>
-      <textarea
-        className="textarea"
-        value={markdown}
-        onChange={(e) => setMarkdown(e.target.value)}
-        style={{ width: "50%" }}
-      />
-
-      <div
-        className="preview"
-        style={{ width: "50%", padding: "20px" }}
-        dangerouslySetInnerHTML={{ __html: preview }}
-      />
-    </div>
-  );
-};
-
+const MarkdownEditor = () => null;
 export default MarkdownEditor;
