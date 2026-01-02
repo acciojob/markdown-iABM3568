11dbfd90953922518d6fa26e24aa0218bccb570b
Abhishek Mishra <iabm3568@gmail.com>
2026-01-02 20:42:46 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,46 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import MarkdownEditor from "./MarkdownEditor";
 
 const App = () => {
-  const [markdown, setMarkdown] = useState("# Hello world");
-  const [preview, setPreview] = useState("");
-
-  useEffect(() => {
-    if (markdown.startsWith("# ")) {
-      setPreview(markdown.replace("# ", ""));
-    } else {
-      setPreview(markdown);
-    }
-  }, [markdown]);
-
-  return (
-    <>
-      {/* loading MUST always exist */}
-      <div className="loading"></div>
-
-      <div className="app" style={{ display: "flex", height: "100vh" }}>
-        {/* LEFT SIDE */}
-        <textarea
-          className="textarea"
-          value={markdown}
-          onChange={(e) => setMarkdown(e.target.value)}
-          style={{ width: "50%" }}
-        />
-
-        {/* RIGHT SIDE */}
-        <div
-          className="preview"
-          style={{
-            width: "50%",
-            padding: "20px",
-            fontSize: "32px",
-            fontWeight: "bold"
-          }}
-        >
-          {preview}
-        </div>
-      </div>
-    </>
-  );
+  return <MarkdownEditor />;
 };
 
 export default App;
