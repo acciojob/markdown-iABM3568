4ab9c90c39c807f66344870b0e099ad06bc84de5
Abhishek Mishra <iabm3568@gmail.com>
2026-01-06 21:14:32 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import MarkdownEditor from "./MarkdownEditor";
 
 const App = () => {
-  return <MarkdownEditor />;
+  return (
+    <div className="app">
+      <MarkdownEditor />
+    </div>
+  );
 };
 
 export default App;
